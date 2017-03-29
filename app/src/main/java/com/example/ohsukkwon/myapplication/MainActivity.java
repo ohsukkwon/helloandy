532dc1e32c83b172808c2298551c6712418f641f
ohsukkwon <osk1004@gmail.com>
2017-03-30 07:30:37 +0900

    Override onStart() in MainActivity.
    Add overriding api(onStart()) in MainActivity.

--- app/src/main/java/com/example/ohsukkwon/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ohsukkwon/myapplication/MainActivity.java
@@ -11,4 +11,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 }
